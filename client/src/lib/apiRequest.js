675d2295595a3b93030420273cc05e8117fe25aa
UtkarshPal45 <utkarshpal2003@gmail.com>
2024-12-25 02:01:46 +0530

    corrected apiRequest.js and added hosted url of backend

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const Backend_URL = process.env.BACKEND_URL 
-    ? `${process.env.BACKEND_URL}/api` 
-    : "http://localhost:8800/api";
-    
 const apiRequest=axios.create({
-    baseURL: Backend_URL,
+    baseURL: "http://localhost:8800/api",
     withCredentials: true,
 })
 
